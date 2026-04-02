2838720741f40f4608b8fa2673c9d0bc7c2ce123
Peter Dinis <petter1@pobx.sk>
2026-04-02 21:41:58 +0200

    feat: Updating next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
+	reactStrictMode: true,
+	cacheComponents: true,
+	experimental: {
+		cssChunking: true
+	}
 };
 
 export default nextConfig;
